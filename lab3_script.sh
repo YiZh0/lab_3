75496ea066bced908f4f2d7d444aa380d43bb25e
“YiZh0” <Yin.zhou@colorado.edu>
2020-02-09 21:29:05 -0700

    Adding all of the files for lab 3

--- lab3_script.sh
+++ lab3_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Authors : Yin Zhou & Your partner's name
+# Authors : Yin Zhou
 # Date: 2/7/2020
 
 #Problem 1 Code:
@@ -8,8 +8,13 @@ echo "Enter a file name: "
 read file_name
 echo "Enter a regular expression: "
 read search_pattern
+echo "Search Result:"
 grep $search_pattern $file_name
+echo "The number of phone numbers in \"regex_practice.txt\":"
 grep -Ec [0-9]{3}\-[0-9]{3}\-[0-9]{4} regex_practice.txt #4.2.3
+echo "The number of emails:"
 grep -Ec .+@.+\. regex_practice.txt #4.2.4.1
+echo "List of all the phone numbers in the \"303\" area code:"
 grep -E -o 303\-[0-9]{3}\-[0-9]{4} regex_practice.txt #4.2.4.2
-grep "@geocities.com" regex_practice.txt > email_results.txt #4.2.4.3
+grep "@geocities.com" regex_practice.txt >> email_results.txt #4.2.4.3
+echo "A list of all the emails from geocities.com is stored in \"email_results.txt\"."
